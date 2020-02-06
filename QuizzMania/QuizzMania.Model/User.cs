bb4b36118a729e38cd8594063b783215432606ad
Sybaris <Sybaris@Club-Internet.Fr>
2020-02-06 14:31:49 +0100

    MVC added

---
+++ QuizzMania/QuizzMania.Model/User.cs
@@ -0,0 +1,38 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace QuizzMania.Model
+{
+    /// <summary>
+    /// User
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// First name
+        /// </summary>
+        [Required]
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Last name
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Email
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Is admin
+        /// </summary>
+        public bool IsAdmin { get; set; }
+    }
+}
