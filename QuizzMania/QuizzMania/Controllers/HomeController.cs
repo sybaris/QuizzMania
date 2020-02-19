045d3b87b8307ec9015cfcccd87bf3d7d2cd5247
Jean-Pierre Planas <sybaris@club-internet.fr>
2020-02-19 18:17:58 +0100

    Refactoring (Redirect + Index --> Login)

--- QuizzMania/QuizzMania/Controllers/HomeController.cs
+++ QuizzMania/QuizzMania/Controllers/HomeController.cs
@@ -13,13 +13,19 @@ namespace QuizzMania.Controllers
     public class HomeController : Controller
     {
         private QuizzManiaContext _quizzManiaContext;
+        
+        /// <summary>
+        /// Constructeur. Les paramètres seront injecté par l'IOC (injection de dépendance)
+        /// </summary>
+        /// <param name="quizzManiaContext"></param>
         public HomeController(QuizzManiaContext quizzManiaContext)
         {
             _quizzManiaContext = quizzManiaContext;
         }
+
         public IActionResult Index()
         {
-            return View();
+            return View("Login");
         }
 
         public IActionResult About()
@@ -30,33 +36,37 @@ namespace QuizzMania.Controllers
         public IActionResult Login(UserViewModel userModel)
         {
             if (!ModelState.IsValid)
-            {
-                return View("Index");
-            }
+                return View("Login");
 
             if (userModel.IsAdmin)
-            {
-                return RedirectToAction("AnswersWhiteboard");
-            }
+                return RedirectToAction("AnswersWhiteboard", "Home", userModel);
 
-            var a = from user in _quizzManiaContext.Users
-                    where user.FirstName.ToLower() == userModel.FirstName.ToLower()
-                    select new UsersViewModel() { FirstName = user.FirstName, Id = user.Id };
-            if (a.Count() != 1)
-                throw new Exception($"L'utilisateur {userModel.FirstName} n'a pas été trouvé");
-            return View("UserSurvey", a.Single());
+            return RedirectToAction("UserSurvey", "Home", userModel);
         }
 
+        public IActionResult UserSurvey(UserViewModel userModel)
+        {
+            var q = from user in _quizzManiaContext.Users
+                    where user.FirstName.ToLower() == userModel.FirstName.ToLower()
+                    select new UsersViewModel() { FirstName = user.FirstName, Id = user.Id };
+            if (q.Count() != 1)
+                throw new Exception($"L'utilisateur {userModel.FirstName} n'a pas été trouvé");
+            return View(q.Single());
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="userModel"></param>
+        /// <returns></returns>
         public IActionResult AnswersWhiteboard(UserViewModel userModel)
         {
             var q = from user in _quizzManiaContext.Users
                     where !user.IsAdmin
                     orderby user.FirstName
                     select new UsersViewModel() { FirstName = user.FirstName, Id = user.Id };
-
-            var result = q.ToList();
-
-            return View("AnswersWhiteboard", result);
+            var players = q.ToList();
+            return View(players);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- QuizzMania/QuizzMania/Models/UserViewModel.cs
+++ QuizzMania/QuizzMania/Models/UserViewModel.cs
@@ -15,19 +15,10 @@ namespace QuizzMania.Models
         [Required (ErrorMessage = "Prénom requis ! ! !"),MinLength(3, ErrorMessage = " Il faut au moins 3 caractères ! ! !")]
         public string FirstName { get; set; }
 
-        ///// <summary>
-        ///// Last name
-        ///// </summary
-        //public string LastName { get; set; }
-
-        ///// <summary>
-        ///// Email
-        ///// </summary>
-        //public string Email { get; set; }
-
         /// <summary>
         /// Is admin
         /// </summary>
+        [Display(Name = "Admin : ")]
         public bool IsAdmin { get; set; }
 
     }
