c94d6aabf074f6e8c956132bbd5384dd590d2158
Sybaris <Sybaris@Club-Internet.Fr>
2023-01-02 21:57:50 +0100

    Migration to dotnet 6.0

--- QuizzMania/QuizzMania.Web/Program.cs
+++ QuizzMania/QuizzMania.Web/Program.cs
@@ -1,32 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using QuizzMania.BusinessLogicLayer;
+using QuizzMania.DataAccessLayer.Context;
+using QuizzMania.DataAccessLayer;
+using QuizzMania.Web.Hubs;
 
-namespace QuizzMania.Web
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<QuizzManiaContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("UserDatabase")));
+builder.Services.AddScoped<IRepository, DBRepository>();
+builder.Services.AddScoped<IBusinessLayer, BusinessLayer>();
+builder.Services.AddSignalR();
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            //Console.WriteLine("JPP Console");
-            //Debug.WriteLine("JPP Debug");
-            //Trace.WriteLine("JPP Trace");
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+    app.UseDeveloperExceptionPage();
+    app.UseMigrationsEndPoint();
 }
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+//app.UseAuthorization();
+
+app.MapHub<QuizzHub>("/QuizzHub");
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
+
+/*
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapHub<QuizzHub>("/QuizzHub");
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+});*/
+//quizzManiaContext.InitDefaultValue();
