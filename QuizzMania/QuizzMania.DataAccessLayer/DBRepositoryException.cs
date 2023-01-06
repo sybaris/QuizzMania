cdfce739d72f68554e71e19191e820992d883083
Sybaris <Sybaris@Club-Internet.Fr>
2023-01-06 09:45:50 +0100

    SonarCloud Code Smell fixes

--- QuizzMania/QuizzMania.DataAccessLayer/DBRepositoryException.cs
+++ QuizzMania/QuizzMania.DataAccessLayer/DBRepositoryException.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace QuizzMania.DataAccessLayer
 {
+    [Serializable]
     public class DBRepositoryException : Exception
     {
         public DBRepositoryException()
